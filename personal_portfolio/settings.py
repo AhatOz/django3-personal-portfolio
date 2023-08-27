90dd1a68c80a6937f249ff49bb4cef7733c34b12
Ahat O <ahatorazgeldiyev@gmail.com>
2023-08-27 13:09:28 +0000

    Remove old stuff

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-st0^d0+%v-2b&#bp5+u^ny64ni)hebcf9@i#u8as01(xt_-ge&"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ahato.pythonanywhere.com']
 
 
 # Application definition
@@ -118,9 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "static"
 
 MEDIA_URL = "media/"
-
 MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
